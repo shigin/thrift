09bff0e389ff60ab5220b8b640a8c00de0e63509
Alexander Shigin <shigin@rambler-co.ru>
2008-07-05 12:10:47 +0400

    mux processor initial

---
+++ lib/py/src/MuxProcess.py
@@ -0,0 +1,38 @@
+from thrift.Thrift import *
+
+class TMuxProcessor(TProcessor):
+    def __init__(self, processors, default=None):
+        """Processor is a dict {'name': processor}"""
+        self.__map = processors
+        self.__default = default
+
+    def __raise_unknown(self, name, iprot, oprot):
+        iprot.skip(TType.STRUCT)
+        iprot.readMessageEnd()
+        x = TApplicationException(
+                TApplicationException.UNKNOWN_METHOD, 'Unknown function %s' % name)
+        oprot.writeMessageBegin(name, TMessageType.EXCEPTION, seqid)
+        x.write(oprot)
+        oprot.writeMessageEnd()
+        oprot.trans.flush()
+
+    def process(self, iprot, oprot):
+        name, type, seqid = iprot.readMessageBegin()
+        if '.' in name:
+            obj_name, name = name.split('.', 1)
+            if obj_name in self.__map:
+                obj = self.__map[obj_name]
+            else:
+                self.__raise_unknown(obj_name, iprot, oprot)
+                return
+        else:
+            if self.__default:
+                obj = self.__default
+            else:
+                self.__raise_unknown(name, iprot, oprot)
+                return
+        if name not in obj._processMap:
+            self.__raise_unknown(name, iprot, oprot)
+        else:
+            obj._processMap[name](self, seqid, iprot, oprot)
+            return True
