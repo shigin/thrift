b8ad9a16b3af18eeeb7f3825925cef5bd35d6674
Alexander Shigin <shigin@rambler-co.ru>
2008-07-07 23:11:22 +0400

    insanity test can work with new style python now

--- lib/py/src/Struct.py
+++ lib/py/src/Struct.py
@@ -6,7 +6,33 @@ try:
 except:
   fastbinary = None
 
-def write_helper(oprot, ftype, value):
+def write_adv_helper(oprot, ftype, type_args, value):
+  if ftype == TType.MAP:
+    # XXX TODO handle complex type
+    ktype, kspec, vtype, vspec = type_args
+    oprot.writeMapBegin(ktype, vtype, len(value))
+    for k, v in value.items():
+      write_helper(oprot, ktype, kspec, k)
+      write_helper(oprot, vtype, vspec, v)
+    oprot.writeMapEnd()
+  elif ftype in (TType.LIST, TType.SET):
+    if ftype == TType.LIST:
+      begin = oprot.writeListBegin
+      end = oprot.writeListEnd
+    else:
+      begin = oprot.writeSetBegin
+      end = oprot.writeSetEnd
+    xtype, xspec = type_args
+    begin(xtype, len(value))
+    for k in value:
+      write_helper(oprot, xtype, xspec, k)
+    end()
+  elif ftype == TType.STRUCT:
+    value.write(oprot)
+  else:
+    raise "XXX: hey, it's %d type" % ftype
+
+def write_helper(oprot, ftype, type_args, value):
   # copy-paste from TProtocol.skip
   if ftype == TType.STOP:
     raise "XXX"
@@ -25,9 +51,9 @@ def write_helper(oprot, ftype, value):
   elif ftype == TType.STRING:
     oprot.writeString(value)
   else:
-    raise "XXX: hey, it's %d type" % ftype
+    write_adv_helper(oprot, ftype, type_args, value)
 
-def reader_helper(iprot, ftype):
+def reader_helper(iprot, ftype, type_args):
   # copy-paste from TProtocol.skip
   if ftype == TType.STOP:
     raise "XXX"
@@ -45,55 +71,42 @@ def reader_helper(iprot, ftype):
     return iprot.readDouble()
   elif ftype == TType.STRING:
     return iprot.readString()
+  elif ftype == TType.STRUCT:
+   class_, _ = type_args
+   result = class_()
+   result.read(iprot)
+   return result
   elif ftype == TType.MAP:
     # XXX TODO: support complex types
     ktype, vtype, size = iprot.readMapBegin()
+    kstype, kspec, vstype, vspec = type_args
+    assert ktype == kstype
+    assert vtype == vstype
     result = {}
     for i in range(size):
-      key = iprot.skip(ktype)
-      value = iprot.skip(vtype)
+      key   = reader_helper(iprot, ktype, kspec)
+      value = reader_helper(iprot, vtype, vspec)
       result[key] = value
     iprot.readMapEnd()
     return result
   elif ftype == TType.SET:
     etype, size = iprot.readSetBegin()
+    stype, spec = type_args
     result = set()
     for i in xrange(size):
-      result.update([reader_helper(iprot, etype)])
+      result.update([reader_helper(iprot, etype, spec)])
     iprot.readSetEnd()
     return result
   elif ftype == TType.LIST:
     etype, size = iprot.readListBegin()
+    stype, spec = type_args
     result = []
     for i in xrange(size):
-      result.append(reader_helper(iprot, etype))
+      result.append(reader_helper(iprot, etype, spec))
     iprot.readListEnd()
     return result
   raise "XXX"
 
-def write_adv_helper(oprot, ftype, type_args, value):
-  if ftype == TType.MAP:
-    # XXX TODO handle complex type
-    ktype, _, vtype, _ = type_args
-    oprot.writeMapBegin(ktype, vtype, len(value))
-    for k, v in value.items():
-      write_helper(oprot, ktype, k)
-      write_helper(oprot, vtype, v)
-    oprot.writeMapEnd()
-  elif ftype in (TType.LIST, TType.SET):
-    if ftype == TType.LIST:
-      begin = oprot.writeListBegin
-      end = oprot.writeListEnd
-    else:
-      begin = oprot.writeSetBegin
-      end = oprot.writeSetEnd
-    xtype, _ = type_args
-    begin(xtype, len(value))
-    for k in value:
-      write_helper(oprot, xtype, k)
-    end()
-  else:
-    raise "XXX"
     
 # new style class has __name__ property
 class ThriftStruct(object):
@@ -101,7 +114,7 @@ class ThriftStruct(object):
   
   Child MUST specify thrift_spec!
   
-   (num, type, typeargs, default)
+   (num, type, type_args, default)
   """
   def __init__(self, vars=None):
     assert self.thrift_spec
@@ -132,13 +145,7 @@ class ThriftStruct(object):
       else:
         stype, sname, type_args, default = self.cached[fid]
         if stype == ftype:
-          if ftype == TType.STRUCT:
-            class_, spec = type_args
-            result = class_()
-            result.read()
-            setattr(self, sname, result)
-          else:
-            setattr(self, sname, reader_helper(iprot, stype))
+          setattr(self, sname, reader_helper(iprot, stype, type_args))
         else:
           iprot.skip(ftype)
     iprot.readFieldEnd()
@@ -158,12 +165,7 @@ class ThriftStruct(object):
       value = getattr(self, fname, default)
       if value is not None: # it's bad idea to skip [] as None
         oprot.writeFieldBegin(fname, ftype, fid)
-        if ftype in (TType.SET, TType.MAP, TType.LIST):
-          write_adv_helper(oprot, ftype, type_args, value)
-        elif ftype == TType.STRUCT:
-          value.write(oprot)
-        else:
-          write_helper(oprot, ftype, value)
+        write_helper(oprot, ftype, type_args, value)
         oprot.writeFieldEnd()
     oprot.writeFieldStop()
     oprot.writeStructEnd()
